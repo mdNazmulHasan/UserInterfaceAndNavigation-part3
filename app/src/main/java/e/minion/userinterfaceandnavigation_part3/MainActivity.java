2f164bbb1219f1fb20f229c5f0c874beba1ee91f
mdNazmulHasan <najmul.hasan10@gmail.com>
2020-12-07 13:34:11 +0600

    displayed alert dialog

--- app/src/main/java/e/minion/userinterfaceandnavigation_part3/MainActivity.java
+++ app/src/main/java/e/minion/userinterfaceandnavigation_part3/MainActivity.java
@@ -1,10 +1,13 @@
 package e.minion.userinterfaceandnavigation_part3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +16,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    public void showAlertDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        builder.setTitle("Are You ok?!");
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
